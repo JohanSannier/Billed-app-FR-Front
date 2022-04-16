347b294565904d032767b8301de787052e452cf0
Johan <johan.sanniercaffier@gmail.com>
2022-04-16 11:50:54 +0200

    Code linté

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -1,64 +1,76 @@
-import { ROUTES_PATH } from '../constants/routes.js'
-import { formatDate, formatStatus } from "../app/format.js"
-import Logout from "./Logout.js"
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { ROUTES_PATH } from '../constants/routes.js';
+import { formatDate, formatStatus } from '../app/format.js';
+import Logout from './Logout.js';
 
 export default class {
   constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
-    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
-    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
-    if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', (e) => this.handleClickIconEye(icon))
-    })
-    new Logout({ document, localStorage, onNavigate })
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.store = store;
+    const buttonNewBill = document.querySelector(
+      `button[data-testid="btn-new-bill"]`
+    );
+    if (buttonNewBill)
+      buttonNewBill.addEventListener('click', this.handleClickNewBill);
+    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
+    if (iconEye)
+      iconEye.forEach((icon) => {
+        icon.addEventListener('click', (e) => this.handleClickIconEye(icon));
+      });
+    new Logout({ document, localStorage, onNavigate });
   }
 
   handleClickNewBill = (e) => {
-    this.onNavigate(ROUTES_PATH['NewBill'])
-  }
+    this.onNavigate(ROUTES_PATH['NewBill']);
+  };
 
   handleClickIconEye = (icon) => {
-    const billUrl = icon.getAttribute("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
-    $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
-    $('#modaleFile').modal('show')
-  }
+    const billUrl = icon.getAttribute('data-bill-url');
+    const imgWidth = Math.floor($('#modaleFile').width() * 0.5);
+    $('#modaleFile')
+      .find('.modal-body')
+      .html(
+        `<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`
+      );
+    $('#modaleFile').modal('show');
+  };
 
   // not need to cover this function by tests
   /* istanbul ignore next */
   getBills = () => {
-    const userEmail = localStorage.getItem('user') ?
-    JSON.parse(localStorage.getItem('user')).email : ""
+    const userEmail = localStorage.getItem('user')
+      ? JSON.parse(localStorage.getItem('user')).email
+      : '';
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then((snapshot) => {
-        const bills = snapshot.map((doc) => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status)
+        .bills()
+        .list()
+        .then((snapshot) => {
+          const bills = snapshot
+            .map((doc) => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status),
+                };
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, 'for', doc);
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status),
+                };
               }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
-          .filter(bill => bill.email === userEmail)
-        return bills
-      })
-      .catch(error => error)
+            })
+            .filter((bill) => bill.email === userEmail);
+          return bills;
+        })
+        .catch((error) => error);
     }
-  }
+  };
 }
